121fefe02d2f7e4ffcbed8bc2a73afa87baa0567
Pranit Yawalkar <pranityawalkar10@gmail.com>
2023-04-30 17:02:18 +0530

    test route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,7 +22,9 @@ import { checkout, verifyPayment } from "../controllers/paymentController.js";
 const router = express.Router();
 
 router.get("/getUsers", getUsers);
-
+router.get("/test", (req, res) => {
+  return res.status(200).send("route is working fine");
+});
 router.get("/wishlist", authMiddleware, getWishlist);
 router.get("/cart", authMiddleware, getUserCart);
 router.get("/orders", authMiddleware, getMyOrders);
